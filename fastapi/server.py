9aff3b9c200a64d7daa7c80f6f9bbf3932938b68
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-12-10 18:15:50 +0700

    Changing bytes.IO

--- fastapi/server.py
+++ fastapi/server.py
@@ -16,20 +16,19 @@ async def root():
 @app.post("/predict")
 async def predict(request: PredictRequest):
     response = requests.get(request.url)
-    image_data = BytesIO(response.content)
-    image = prediction.preprocess_image(image_data)
+    image = prediction.read_image(response)
+    image = prediction.preprocess_image(image)
     predict = prediction.predict_image(image)
     print(f"Image URL: {request.url}, Predicted: {predict}")
     return predict
 
-@app.post("/predic-file")
+@app.post("/predict-file")
 async def predict(file: UploadFile = File(...)):
     image = prediction.read_image(await file.read())
     image = prediction.preprocess_image(image)
     predict = prediction.predict_image(image)
     print(predict)
     return predict
-
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
